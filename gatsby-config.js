ab8c894ede781ff842d63c0e293b1f37c98d365b
timscriv <tscrivner1@gmail.com>
2019-12-22 09:15:15 -0600

    feature(gatsby-update): migrate root of blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,44 +1,46 @@
-const lost = require('lost')
-const pxtorem = require('postcss-pxtorem')
+'use strict'
+
+const siteConfig = require('./config.js')
+const postCssPlugins = require('./postcss-config.js')
 
 module.exports = {
+  pathPrefix: siteConfig.pathPrefix,
   siteMetadata: {
-    url: 'https://timscriv.com',
-    title: 'Tim Scriv',
-    subtitle: 'Dad, husband, coder, barbequer, problem-solver.',
-    copyright: '© Tim Scrivner',
-    disqusShortname: 'timscriv',
-    menu: [
-      {
-        label: 'Articles',
-        path: '/',
-      },
-    ],
-    author: {
-      name: 'Tim Scriv',
-      email: 'tim@timscriv.com',
-      twitter: 'timscriv',
-      github: 'timscriv',
-    },
+    url: siteConfig.url,
+    title: siteConfig.title,
+    subtitle: siteConfig.subtitle,
+    copyright: siteConfig.copyright,
+    disqusShortname: siteConfig.disqusShortname,
+    menu: siteConfig.menu,
+    author: siteConfig.author,
   },
   plugins: [
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: 'timscriv',
-        short_name: 'timscriv',
-        start_url: '/',
-        background_color: '#ffffff',
-        theme_color: '#25536D',
-        display: 'standalone',
-        icon: 'src/assets/logos/logo.svg', // This path is relative to the root of the site.
+        path: `${__dirname}/content`,
+        name: 'pages',
       },
     },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
-        path: `${__dirname}/src/pages`,
-        name: 'pages',
+        path: `${__dirname}/static/media`,
+        name: 'media',
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'css',
+        path: `${__dirname}/static/css`,
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'assets',
+        path: `${__dirname}/static`,
       },
     },
     {
@@ -58,21 +60,20 @@ module.exports = {
         feeds: [
           {
             serialize: ({ query: { site, allMarkdownRemark } }) =>
-              allMarkdownRemark.edges.map(edge =>
-                Object.assign({}, edge.node.frontmatter, {
-                  description: edge.node.frontmatter.description,
-                  date: edge.node.frontmatter.date,
-                  url: site.siteMetadata.site_url + edge.node.fields.slug,
-                  guid: site.siteMetadata.site_url + edge.node.fields.slug,
-                  custom_elements: [{ 'content:encoded': edge.node.html }],
-                })
-              ),
+              allMarkdownRemark.edges.map(edge => ({
+                ...edge.node.frontmatter,
+                description: edge.node.frontmatter.description,
+                date: edge.node.frontmatter.date,
+                url: site.siteMetadata.site_url + edge.node.fields.slug,
+                guid: site.siteMetadata.site_url + edge.node.fields.slug,
+                custom_elements: [{ 'content:encoded': edge.node.html }],
+              })),
             query: `
               {
                 allMarkdownRemark(
                   limit: 1000,
                   sort: { order: DESC, fields: [frontmatter___date] },
-                  filter: { frontmatter: { layout: { eq: "post" }, draft: { ne: true } } }
+                  filter: { frontmatter: { template: { eq: "post" }, draft: { ne: true } } }
                 ) {
                   edges {
                     node {
@@ -83,7 +84,7 @@ module.exports = {
                       frontmatter {
                         title
                         date
-                        layout
+                        template
                         draft
                         description
                       }
@@ -93,6 +94,7 @@ module.exports = {
               }
             `,
             output: '/rss.xml',
+            title: siteConfig.title,
           },
         ],
       },
@@ -101,65 +103,93 @@ module.exports = {
       resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
+          'gatsby-remark-relative-images',
+          {
+            resolve: 'gatsby-remark-katex',
+            options: {
+              strict: 'ignore',
+            },
+          },
           {
             resolve: 'gatsby-remark-images',
             options: {
               maxWidth: 960,
+              withWebp: true,
+              ignoreFileExtensions: [],
             },
           },
           {
             resolve: 'gatsby-remark-responsive-iframe',
             options: { wrapperStyle: 'margin-bottom: 1.0725rem' },
           },
+          'gatsby-remark-autolink-headers',
           'gatsby-remark-prismjs',
           'gatsby-remark-copy-linked-files',
           'gatsby-remark-smartypants',
+          'gatsby-remark-external-links',
         ],
       },
     },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
+    'gatsby-plugin-netlify',
     {
-      resolve: 'gatsby-plugin-google-analytics',
-      options: { trackingId: 'UA-126708983-1' },
+      resolve: 'gatsby-plugin-netlify-cms',
+      options: {
+        modulePath: `${__dirname}/src/cms/index.js`,
+      },
     },
     {
-      resolve: 'gatsby-plugin-google-fonts',
+      resolve: 'gatsby-plugin-google-gtag',
       options: {
-        fonts: ['roboto:400,400i,500,700'],
+        trackingIds: [siteConfig.googleAnalyticsId],
+        pluginConfig: {
+          head: true,
+        },
       },
     },
     {
       resolve: 'gatsby-plugin-sitemap',
       options: {
         query: `
-            {
-              site {
-                siteMetadata {
-                  url
+          {
+            site {
+              siteMetadata {
+                siteUrl: url
+              }
+            }
+            allSitePage(
+              filter: {
+                path: { regex: "/^(?!/404/|/404.html|/dev-404-page/)/" }
+              }
+            ) {
+              edges {
+                node {
+                  path
                 }
               }
-              allSitePage(
-                filter: {
-                  path: { regex: "/^(?!/404/|/404.html|/dev-404-page/)/" }
-                }
-              ) {
-                edges {
-                  node {
-                    path
-                  }
-                }
-              }
-          }`,
+            }
+          }
+        `,
         output: '/sitemap.xml',
         serialize: ({ site, allSitePage }) =>
-          allSitePage.edges.map(edge => {
-            return {
-              url: site.siteMetadata.url + edge.node.path,
-              changefreq: 'daily',
-              priority: 0.7,
-            }
-          }),
+          allSitePage.edges.map(edge => ({
+            url: site.siteMetadata.siteUrl + edge.node.path,
+            changefreq: 'daily',
+            priority: 0.7,
+          })),
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: siteConfig.title,
+        short_name: siteConfig.title,
+        start_url: '/',
+        background_color: '#FFF',
+        theme_color: '#25536D',
+        display: 'standalone',
+        icon: 'static/logo.svg',
       },
     },
     'gatsby-plugin-offline',
@@ -168,38 +198,13 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-sass',
       options: {
-        postCssPlugins: [
-          lost(),
-          pxtorem({
-            rootValue: 16,
-            unitPrecision: 5,
-            propList: [
-              'font',
-              'font-size',
-              'line-height',
-              'letter-spacing',
-              'margin',
-              'margin-top',
-              'margin-left',
-              'margin-bottom',
-              'margin-right',
-              'padding',
-              'padding-top',
-              'padding-left',
-              'padding-bottom',
-              'padding-right',
-              'border-radius',
-              'width',
-              'max-width',
-            ],
-            selectorBlackList: [],
-            replace: true,
-            mediaQuery: false,
-            minPixelValue: 0,
-          }),
-        ],
-        precision: 8,
+        postCssPlugins: [...postCssPlugins],
+        cssLoaderOptions: {
+          camelCase: false,
+        },
       },
     },
+    'gatsby-plugin-flow',
+    'gatsby-plugin-optimize-svgs',
   ],
 }
